f75147f1cae94d63dbccf0e97f7d1fd63f44e00f
Detrix <kykot.oleg@gmail.com>
2018-12-28 14:21:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Linete</title>
+        
+        <link href="style/bootstrap-theme.css" type="text/css" rel="stylesheet" />
+        <link href="style/bootstrap.css" type="text/css" rel="stylesheet" />
+        <link href="style/bootstrap-theme.min.css" type="text/css" rel="stylesheet" />
+        <link href="style/bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <link href="style/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <img class="logo" src="images/logo.png" alt="Linette" title="Linette">
+        </div>
+        <div class="top_menu">
+            <ul class="nav navbar-nav">
+              <li><a href="#"><span>Кулинария</span></a></li>
+              <li><a href="#"><span>Мода</span></a></li>
+              <li><a href="#"><span>Красота</span></a></li>
+              <li><a href="#"><span>Мамочкам</span></a></li>
+              <li><a href="#"><span>Фитнесс</span></a></li>
+              <li><a href="#"><span>Животные</span></a></li>
+              <li><a href="#"><span>Психология</span></a></li>
+              <li><a href="#"><span>Здоровье</span></a></li>
+            </ul>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+              <li><div id="burger"><img src="images/menu.png" alt="MENU"></div></li>
+              <li><span></span></li>
+            </ul>
+      </div>
+    </nav>
+    <div id="container">
+        <div id="wrapper">
+            <div id="main_menu">
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/1.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                          <h2>Кулинария</h2>
+                          <p>Есть люди - которые живут ради еды! И это неудевительно, ведь в мире существует множество изумительных рецептов. Их вы сможете найти в этом разделе.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/2.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Мода</h2>
+                            <p>Встречают по одежке, а провожают по уму.Согласитесь, хорошо выглядить это очень важно, особенно в наше время.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/3.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Красота</h2>
+                            <p>Если некрасива девочка, то в этом виновата природа, если некрасива женщина - в этом виновата она сама.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/4.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Мамочкам</h2>
+                            <p>Быть по-матерински заботливым - значить быть способным на безусловную любовь. Секреты воспитания и не только</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/5.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Спорт</h2>
+                            <p>В здоровом теле - здоровый дух. Это абсолютно верно, ведь достаточно всего пару минут в день дабы укрепить наше тело.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/6.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Домашние питомцы</h2>
+                            <p>Мы в ответе за тех кого приручили, и обязаны хнать все тонкости об уходе за ними.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/7.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Психология</h2>
+                            <p>Иногда для умиротворения достаточно просто включить любимую музыку и заварить чашку чая. А что нужно Вам?</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                <div id="photo">
+                    <div class="flip-box">
+                      <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <a href="#"><img href="#" src="images/8.jpg" id="pimg" class="img-thumbnail" alt="1"></a>
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Здоровье</h2>
+                            <p>Здоровье -  дороже золота. И мы обязаны ценить его, ведь здоровый человек -  здоровые мысли.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/script.js"></script>
+<footer class="container-fluid bg-4 text-center">
+  <p>Home <a href="#">powered in 2018</a></p> 
+</footer>
+</body>
+</html>
